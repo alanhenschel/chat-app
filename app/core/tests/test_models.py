7e4947c2906998000931a524a90adbd682664883
Alanhenschel <alanhenschel2@gmail.com>
2022-03-06 17:06:47 -0300

    wait db feature and test for users

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,44 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating new user with an email"""
+        email = 'test@recipepython.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_email_normalize(self):
+        """Test creating new user with an email normalize"""
+        email = 'test@recipepythoNNN.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        user = get_user_model().objects.create_superuser(
+            'teste@testepython.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
